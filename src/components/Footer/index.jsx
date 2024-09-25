93f6c15e72c539788f605124c73aa3814b2aeb76
Elea Pimouguet <elea.pimouguet@gmail.com>
2024-09-25 20:05:08 +0200

    build: footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <section
       id="footer"
-      className="h-[120px] w-full flex justify-center items-center custom-card"
+      className="h-[120px] w-full flex flex-col justify-center items-center custom-card"
     >
       <span className="text-base font-thin">
         © 2024 Eléa Pimouguet. All rights reserved.
